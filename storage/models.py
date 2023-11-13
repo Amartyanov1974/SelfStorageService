1272baa5df62ad7a1275f3922e1b9d9ac0423e47
Amartyanov1974 <randomquake@yandex.ru>
2023-11-13 17:02:00 +0400

    Добавил модель client

--- storage/models.py
+++ storage/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Client(models.Model):
+    user = models.OneToOneField(User,
+                                verbose_name='Клиент',
+                                on_delete=models.CASCADE,
+                                primary_key=True,
+                                related_name='client')
+    address = models.TextField(verbose_name='Адрес клиента',
+                               blank=True,
+                               null=True)
+    phonenumber = models.CharField(verbose_name='Номер телефона',
+                                   max_length=50)
+    class Meta:
+        verbose_name = 'клиент'
+        verbose_name_plural = 'клиенты'
+
+    def __str__(self):
+        return f'{self.user}: {self.address}, {self.phonenumber}'
