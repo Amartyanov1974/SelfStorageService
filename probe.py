19649300b187fdb650ef2f76e90e79464f6661bf
Dmitrii Masliaev <m6ytcbi@gmail.com>
2023-11-18 00:20:29 +0300

    Удалил лишний файл

--- probe.py
+++
@@ -1,7 +0,0 @@
-import random
-
-s = Storage.objects.all()[3]
-
-for i in range(1, 11):
-    Box.objects.create(name=i, storage=s, length=random.randint(2, 4), width=random.randint(2, 4),
-                       height=random.randint(2, 4), price=random.randint(2000, 4000))
